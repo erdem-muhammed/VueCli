9d4b250910c6db5b1ff9654ee94292acfd64f4eb
muhammed erdem <erdem.muhammed1@outlook.com>
2021-10-09 22:28:09 +0200

    const users added

--- SocialMediaApp/socialmedia/src/components/profile/post.vue
+++ SocialMediaApp/socialmedia/src/components/profile/post.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <div class="text">
-            Contributions
+            Contributions 
         </div>
         <div class="postContainer">
             asdknbasjd
--- SocialMediaApp/socialmedia/src/views/User.vue
+++ SocialMediaApp/socialmedia/src/views/User.vue
@@ -6,9 +6,19 @@
         </div>
             <Posts />
     </div>
+    
 </template>
 
 <script>
+const users =
+[
+    { id: 0, username: "max_muster40", fullname: "Max Mustermann", email: "user@gmail.com"},
+    { id: 1, username: "beauty_lena", fullname: "Lena Schmidt", email: "user@gmail.com"},
+    { id: 2, username: "serios_sam", fullname: "Sam Schmitz", email: "user@gmail.com"},
+    { id: 3, username: "Johnny_Bravo", fullname: "Johnny Bravo", email: "user@gmail.com"},
+    { id: 4, username: "Adler_Tim", fullname: "Tim Adler", email: "user@gmail.com"},
+    { id: 5, username: "Endless", fullname: "Charlotte Krüger", email: "user@gmail.com"}
+]
 import Photo from "../components/profile/photo.vue"
 import Title from "../components/profile/title.vue"
 import Posts from "../components/profile/post.vue"
@@ -20,6 +30,10 @@ export default
     Photo,
     Title,
     Posts
+  }
+  computed:
+  {
+      
   }
 } 
 </script>
