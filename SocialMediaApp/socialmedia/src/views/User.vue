a296f5dbfde77cb45e47ff227a9a5262be245a10
muhammed erdem <erdem.muhammed1@outlook.com>
2021-10-09 21:38:10 +0200

    user template phot,title, post created, import-export made

---
+++ SocialMediaApp/socialmedia/src/views/User.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <div class="top">
+            <Photo />
+            <Title />
+        </div>
+            <Posts />
+    </div>
+</template>
+
+<script>
+import Photo from "../components/profile/photo.vue"
+import Title from "../components/profile/title.vue"
+import Posts from "../components/profile/post.vue"
+
+export default 
+{
+  components: 
+  {
+    Photo,
+    Title,
+    Posts
+  }
+} 
+</script>
+
+<style scoped>
+
+</style>
