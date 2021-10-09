da6fee3fc52da674c3163173cdb523622f7dc1b8
muhammed erdem <erdem.muhammed1@outlook.com>
2021-10-09 21:41:00 +0200

    .wrapper defined to App.vue

---
+++ SocialMediaApp/socialmedia/src/components/profile/photo.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        Photo
+    </div>
+</template>
+
+
+<style scoped>
+div
+{
+    width: 200px;
+    height: 200px;
+    border-radius: 50%;
+    background-color: #ccc;
+}  
+</style>
