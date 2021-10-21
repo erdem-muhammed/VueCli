b5e99b3cb0eb3df9f81469f26503a399f841a9a5
muhammed erdem <erdem.muhammed1@outlook.com>
2021-10-21 18:47:00 +0200

    loginContainer created

--- cookies/src/views/User.vue
+++ cookies/src/views/User.vue
@@ -1,5 +1,10 @@
 <template>
-    <div>
-        User Vue
+    <div class="container wrapper">
+        <h2>Welcome</h2>
+        <div class="loginContainer">
+            <input placeholder="Username" id="uName">
+            <input placeholder="Password" id="pw" type="password">
+            <button>Login</button>
+        </div>
     </div>
 </template>
