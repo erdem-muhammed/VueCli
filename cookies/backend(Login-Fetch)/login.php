fe0ddebf67f93973637e7bb055777623c457a475
muhammed erdem <erdem.muhammed1@outlook.com>
2021-10-23 20:02:11 +0200

    username and password added

--- cookies/backend(Login-Fetch)/login.php
+++ cookies/backend(Login-Fetch)/login.php
@@ -8,4 +8,29 @@
      header('Access-Control-Max-Age: 86400');
  }
  //------------------------
+
+
+
+
+ 
+ if(isset($_GET["username"]) && isset($_GET["password"]))
+ {
+     $b = $_GET["username"];
+     $p = $_GET["password"];
+
+     if($b == "admin" && $p == "123456")
+     {
+         echo("1");
+     }
+     else
+     {
+         echo("0");
+     }
+
+ }
+ else
+ {
+     echo("Parameter not complete ");
+ }
+
  ?>
