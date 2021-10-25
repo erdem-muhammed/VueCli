a897df810b10bd0a0581b0abfe582bcb0baf18d6
muhammed erdem <erdem.muhammed1@outlook.com>
2021-10-25 21:10:15 +0200

    initRate() updated

--- vuex-fetch-bitcoin/src/store/index.js
+++ vuex-fetch-bitcoin/src/store/index.js
@@ -18,8 +18,14 @@ export default new Vuex.Store({
     {
       let response = await fetch("https://api.coindesk.com/v1/bpi/currentprice.json");
       let result = await response.json();
-    }
+      let i = 0;
 
+      for(let a in result.bpi)
+      {
+        context.commit("addRate", {id: i, name: result.bpi[a].code, kurs: result.bpi[a].rate_float})
+          i++;
+      }
+    }
   },
   modules: {
   }
