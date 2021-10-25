c1b857be577f2479c52c138eb22fc755d1fd9598
muhammed erdem <erdem.muhammed1@outlook.com>
2021-10-25 20:59:15 +0200

    app updated

--- vuex-fetch-bitcoin/src/App.vue
+++ vuex-fetch-bitcoin/src/App.vue
@@ -1,7 +1,13 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  Current Bitcoin Rate:
+  <ul>
+    <li>
+      {{ c.rate }}  {{ c.name }}
+    </li>
+  </ul>
+
+  Current EUR: <span> {{ euroRate.rate }} </span>
   </div>
 </template>
 
