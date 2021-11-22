7150d0b42cb89ca8d3ded49032921a034beb60c0
muhammed erdem <erdem.muhammed1@outlook.com>
2021-11-22 21:05:17 +0100

    header.vue created

--- to-do_list/src/App.vue
+++ to-do_list/src/App.vue
@@ -1,9 +1,7 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+ <h1>
+ </h1>
     <router-view/>
   </div>
 </template>
