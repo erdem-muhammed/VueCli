9b28b3e90be1760ee7083302d6bb1aae31bb52d9
muhammed erdem <erdem.muhammed1@outlook.com>
2021-11-22 21:08:56 +0100

    header created

---
+++ to-do_list/src/components/header.vue
@@ -0,0 +1,39 @@
+<template>
+    <header>
+        <div class="wrapper">
+            <div class="menu">
+                <div id="logo">
+                    <h1>My Website</h1>
+                </div>
+                <div id="nav">
+                    <router-link to="/">To-Do's</router-link> 
+                    <router-link to="/shoppingList">Shopping List</router-link>
+                </div>
+            </div>
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+header
+{
+    background-color: #111;
+}
+.wrapper
+{
+    max-width: 1000px;
+    margin: 0 auto;
+}
+.menu
+{
+    display: flex;
+    justify-content: space-between;
+}
+
+</style>
