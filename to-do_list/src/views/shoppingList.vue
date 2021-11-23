5a187a06962be70a37a98db42e9aea2583e54ebc
muhammed erdem <erdem.muhammed1@outlook.com>
2021-11-23 16:30:18 +0100

    import topMenu from header.vue

---
+++ to-do_list/src/views/shoppingList.vue
@@ -0,0 +1,12 @@
+<template>
+    <topMenu></topMenu>
+    
+</template>
+
+<script>
+// eslint-disable-next-line no-unused-vars
+import topMenu from "../components/header.vue"
+export default {
+    
+}
+</script>
