0ca57b5323266c1e475b84cc29341e05220cadac
muhammed erdem <erdem.muhammed1@outlook.com>
2021-11-23 16:46:49 +0100

    draft prepared

--- to-do_list/src/components/toDo.vue
+++ to-do_list/src/components/toDo.vue
@@ -1,6 +1,14 @@
 <template>
     <div class="container">
-        
+        <div class="inputs">
+            <input type="text" placeholder="Enter a task">
+            <button>ADD</button>
+        </div>
+        <div class="toDos">
+            <ul>
+                <li></li>
+            </ul>
+        </div>
 
     </div>
 </template>
