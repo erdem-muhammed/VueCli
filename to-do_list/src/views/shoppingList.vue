5099e3b65498a52818757081246a70989f6ed2a9
muhammed erdem <erdem.muhammed1@outlook.com>
2021-11-23 16:44:22 +0100

    toDo imported from toDo.vue

--- to-do_list/src/views/shoppingList.vue
+++ to-do_list/src/views/shoppingList.vue
@@ -1,12 +1,15 @@
 <template>
+<div class="wrapper main">
     <topMenu></topMenu>
-    
+    <toDo></toDo>
+</div>
 </template>
 
 <script>
 // eslint-disable-next-line no-unused-vars
-import topMenu from "../components/header.vue"
+import topMenu from "../components/header.vue";
+import toDo from "../components/toDo.vue";
 export default {
     
 }
-</script>
+</script>ToDo
