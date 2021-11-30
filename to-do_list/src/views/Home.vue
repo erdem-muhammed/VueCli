de98d452bd9ae7073ec893b2d87841f63165be6e
muhammed erdem <erdem.muhammed1@outlook.com>
2021-11-30 15:16:47 +0100

    erase function added

--- to-do_list/src/views/Home.vue
+++ to-do_list/src/views/Home.vue
@@ -1,13 +1,22 @@
 <template>
-
+<div class="wrapper main">
+    <topMenu></topMenu>
+    <toDo></toDo>
+</div>
 </template>
 
 <script>
 // @ is an alias to /src
 
 
+import topMenu from "../components/header.vue";
+import toDo from "../components/toDo.vue";
 export default {
-  name: 'Home',
-  
+    name: 'home',
+    components: {
+        topMenu,
+        toDo
+    }
+    
 }
 </script>
